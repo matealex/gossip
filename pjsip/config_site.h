a4717b669a77dd31fb4b696192a360efe887cd7d
matealex <alexandru_matei2000@yahoo.com>
2017-07-12 13:19:17 +0300

    Update pjsip v2.6 - @5625
    Enable IPV6 and OPUS codec

--- pjsip/config_site.h
+++ pjsip/config_site.h
@@ -1,6 +1,9 @@
 #define PJ_CONFIG_IPHONE 1
-#include <pj/config_site_sample.h>
+#define PJMEDIA_HAS_SPEEX_CODEC 0
+#define PJMEDIA_HAS_GSM_CODEC 0
+#define PJMEDIA_HAS_G722_CODEC 0
+#define PJMEDIA_HAS_OPUS_CODEC 1
+#define PJ_HAS_IPV6 1
 
-// enable G729
-#define PJMEDIA_HAS_G729_CODEC 1
+#include <pj/config_site_sample.h>
 
